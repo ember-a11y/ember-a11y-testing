2fdb3fcf011708a23432e8b6b5ffd198f4b71095
Trent Willis <trentmwillis@gmail.com>
2015-06-25 19:51:48 -0700

    Exclude axe-component from production builds

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 var path = require('path');
 var fs = require('fs');
+var Funnel = require('broccoli-funnel');
+var VersionChecker = require('ember-cli-version-checker');
 
 // The different types/area for which we have content for.
 var ALLOWED_CONTENT_FOR = [
@@ -36,5 +38,24 @@ module.exports = {
     if (~ALLOWED_CONTENT_FOR.indexOf(type)) {
       return fs.readFileSync(path.join(__dirname, 'content-for', type + '.html'));
     }
+  },
+
+  /**
+   * Exclude the self-auditing components code during build if this is a
+   * production build or if the version of Ember being used is less than 1.13.
+   * @override
+   */
+  treeForApp: function(tree) {
+    var checker = new VersionChecker(this);
+    var isProductionBuild = this.app.env === 'production';
+    var isOldEmber = checker.for('ember', 'bower').lt('1.13.0');
+
+    if (isProductionBuild || isOldEmber) {
+      tree = new Funnel(tree, {
+        exclude: [/instance-initializers\/axe-component.js/]
+      });
+    }
+
+    return tree;
   }
 };
