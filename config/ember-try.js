95abe929554855882559fcbdb536c9958f028ac5
Melanie Sumner <melaniersumner@gmail.com>
2019-05-06 10:30:24 -0500

    removed use of/support for node 6 & updated travis config and ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,18 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
@@ -38,11 +26,23 @@ module.exports = function() {
         {
           name: 'ember-lts-3.4',
           env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~3.4.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.8',
+          env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': false }),
           },
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.8.0'
             }
           }
         },
