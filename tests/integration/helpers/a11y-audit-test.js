cce7000ad8c76924049715aec4de6e4b372cdd0b
Melanie Sumner <melaniersumner@gmail.com>
2019-05-14 09:35:11 -0500

    moved jQuery guard to the correct place

--- tests/integration/helpers/a11y-audit-test.js
+++ tests/integration/helpers/a11y-audit-test.js
@@ -7,20 +7,18 @@ import a11yAudit from 'ember-a11y-testing/test-support/audit';
 module('Integration | Helper | a11yAudit', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('a11yAudit runs successfully with jquery context', async function(assert) {
-    await render(hbs`{{#axe-component}}{{/axe-component}}`);
-    await a11yAudit(this.$());
-    assert.ok(true, 'a11yAudit ran and didn\'t find any issues');
-  });
+  if (typeof jQuery != 'undefined') {
+    test('a11yAudit runs successfully with jquery context', async function(assert) {
+      await render(hbs`{{#axe-component}}{{/axe-component}}`);
+      await a11yAudit(this.$());
+      assert.ok(true, 'a11yAudit ran and didn\'t find any issues');
+    });
+  } 
 
   test('a11yAudit runs successfully with element context', async function(assert) {
-    if (typeof jQuery != 'undefined') {
-      await render(hbs`{{#axe-component}}{{/axe-component}}`);
-      await a11yAudit(this.element);
-      assert.ok(true, 'a11yAudit ran and didn\'t find any issues');
-    } else {
-      assert.ok(true, 'a11yAudit did not run this test because jQuery is not present');
-    }
+    await render(hbs`{{#axe-component}}{{/axe-component}}`);
+    await a11yAudit(this.element);
+    assert.ok(true, 'a11yAudit ran and didn\'t find any issues');
   });
 
   test('a11yAudit catches violations successfully', async function(assert) {
