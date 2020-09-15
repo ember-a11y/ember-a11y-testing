6b8969dc56ca77d51e9cc6b37ae9bc896e79e226
Steve Calvert <scalvert@linkedin.com>
2020-09-15 09:25:18 -0700

    Cleanup based on PR comments

--- addon-test-support/setup-middleware-reporter.ts
+++ addon-test-support/setup-middleware-reporter.ts
@@ -12,8 +12,8 @@ import { DEBUG } from '@glimmer/env';
 interface AxeTestResult {
   moduleName: string;
   testName: string;
-  urls: string[];
-  routes: string[];
+  urls: string[] | Set<string>;
+  routes: string[] | Set<string>;
   helpers: string[];
   stack: string;
   violations: Result[];
@@ -22,7 +22,9 @@ interface AxeTestResult {
 export const TEST_SUITE_RESULTS: AxeTestResult[] = [];
 
 let currentTestResult: AxeTestResult | undefined = undefined;
-let currentViolationsMap = new Map<string, Result>();
+let currentViolationsMap: Map<string, Result> | undefined = undefined;
+let currentUrls = new Set<string>();
+let currentRouteNames = new Set<string>();
 
 export async function middlewareReporter(axeResults: AxeResults) {
   if (axeResults.violations.length === 0) {
@@ -38,22 +40,24 @@ export async function middlewareReporter(axeResults: AxeResults) {
     currentTestResult = {
       moduleName: module.name,
       testName,
-      urls: new Set(),
-      routes: new Set(),
+      urls: [],
+      routes: [],
       helpers: testMetaData.usedHelpers,
       stack,
       violations: [],
     };
+
+    currentViolationsMap = new Map();
   }
 
-  currentTestResult.urls.add(currentURL());
-  currentTestResult.routes.add(currentRouteName());
+  currentUrls.add(currentURL());
+  currentRouteNames.add(currentRouteName());
 
   axeResults.violations.forEach((violation) => {
-    let rule = currentViolationsMap.get(violation.id);
+    let rule = currentViolationsMap!.get(violation.id);
 
     if (rule === undefined) {
-      currentViolationsMap.set(violation.id, violation);
+      currentViolationsMap!.set(violation.id, violation);
     } else {
       rule.nodes.push(...violation.nodes);
     }
@@ -62,13 +66,13 @@ export async function middlewareReporter(axeResults: AxeResults) {
 
 export function pushTestResult() {
   if (typeof currentTestResult !== 'undefined') {
-    currentTestResult.violations = [...currentViolationsMap.values()];
-    currentTestResults.urls = [...currentTestResults.urls.values()]);
-    currentTestResults.routes = [...currentTestResults.routes.values()]);
+    currentTestResult.violations = [...currentViolationsMap!.values()];
+    currentTestResult.urls = [...currentUrls.values()];
+    currentTestResult.routes = [...currentRouteNames.values()];
     TEST_SUITE_RESULTS.push(currentTestResult);
 
     currentTestResult = undefined;
-    currentViolationsMap = new Map();
+    currentViolationsMap = undefined;
   }
 }
 
