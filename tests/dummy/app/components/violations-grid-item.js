a3d94981f48e5b7e96384e43a055540cab87e685
drewlee <andlee@linkedin.com>
2021-04-19 14:30:29 -0700

    Rebase, allow all try:each scenarios to pass

--- tests/dummy/app/components/violations-grid-item.js
+++ tests/dummy/app/components/violations-grid-item.js
@@ -1,3 +1,6 @@
+// Temporary until Ember 3.8 & 3.12 support is dropped,
+// afterwhich can be replaced w/templateOnly.
+/* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
 
 export default class ViolationsGridItem extends Component {}
