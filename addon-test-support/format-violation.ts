417f4e57fb8b6263a2ce7d874984aa91a6b2e700
Steve Calvert <scalvert@linkedin.com>
2021-07-01 10:37:19 -0700

    Updating jsdoc

--- addon-test-support/format-violation.ts
+++ addon-test-support/format-violation.ts
@@ -3,8 +3,8 @@ import type { Result } from 'axe-core';
 /**
  * Formats the axe violation for human consumption
  *
- * @param {AxeViolation} violation
- * @param {string | string[]} markup (optional) string of HTML relevant to the violation
+ * @param {Partial<Result>} violation
+ * @param {string[]} markup (optional) string of HTML relevant to the violation
  */
 export default function formatViolation(
   violation: Partial<Result>,
