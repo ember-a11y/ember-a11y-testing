cfda25ef9565622d61f64f1c5f1d1bacb811add5
drewlee <andlee@linkedin.com>
2023-02-01 11:50:32 -0800

    Fixes currentRouteName middleware reporter test failures

---
+++ tests/unit/get-current-route-name-test.ts
@@ -0,0 +1,37 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { _getCurrentRouteName } from 'ember-a11y-testing/test-support/setup-middleware-reporter';
+
+module('Unit | Utils | getCurrentRouteName', function (hooks) {
+  setupTest(hooks);
+
+  test('gets the route name for the current test', function (assert) {
+    function mockCurrentRouteName(): string {
+      return 'index';
+    }
+
+    const result = _getCurrentRouteName(mockCurrentRouteName);
+
+    assert.strictEqual(result, 'index');
+  });
+
+  test('absorbs `currentRouteName` error when route name is null', function (assert) {
+    function currentRouteNameMock(): string {
+      throw new Error('currentRouteName shoudl be a string');
+    }
+
+    const result = _getCurrentRouteName(currentRouteNameMock);
+
+    assert.strictEqual(result, '');
+  });
+
+  test('bubbles up all other emitted errors', function (assert) {
+    function mockCurrentRouteName(): string {
+      throw new Error('Catastrophic error!');
+    }
+
+    assert.throws(() => {
+      _getCurrentRouteName(mockCurrentRouteName);
+    }, /Catastrophic error!/);
+  });
+});
